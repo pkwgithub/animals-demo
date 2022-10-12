74071d029608924daa5c5c9ca49d87d170a64e63
潘柯伟 <1347464914@qq.com>
2022-10-12 17:17:40 +0800

    宠物狗页面

--- src/main/java/com/animals/animalsdemo/unit/EasyExcelDataListener.java
+++ src/main/java/com/animals/animalsdemo/unit/EasyExcelDataListener.java
@@ -6,7 +6,6 @@ import com.alibaba.excel.util.ListUtils;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 
 @Slf4j
 public class EasyExcelDataListener {
